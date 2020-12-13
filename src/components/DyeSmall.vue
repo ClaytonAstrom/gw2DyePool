c53ad854324f8f8b7150f5ff81707f6685562815
Clayton Astrom <clayton.astrom@morningstar.com>
2020-12-13 15:29:44 -0600

    Created a basic data relationship

---
+++ src/components/DyeSmall.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="color.icon" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: {
+    color: Object,
+  },
+})
+export default class DyeGrid extends Vue {}
+</script>
